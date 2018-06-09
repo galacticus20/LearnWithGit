583a18d79b21dc2f42f4165c72180260d2066cda
galacticus20 <galacticus20.gmail.com>
2018-06-09 07:51:04 -0400

    Manual merge

--- c.pp
+++ c.pp
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
  edycja pliku ccc
 
 to bedzie druga modyfikacja pliku c
->>>>>>> 3084c44... Second edit file c
-=======
- edycja pliku ccc
-
-to bedzie druga modyfikacja pliku c
->>>>>>> d642524eae45107c7910197024f2d068b9bcaf21
